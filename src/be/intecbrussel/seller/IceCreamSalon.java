43027550b402fa0b724cc18d12b40c1a831ad78d
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 21:10:33 +0200

    VII

--- src/be/intecbrussel/seller/IceCreamSalon.java
+++ src/be/intecbrussel/seller/IceCreamSalon.java
@@ -27,34 +27,6 @@ public class IceCreamSalon implements IceCreamSeller {
 
         return hoorntje;
 
-
-
-
-
-
-
-
-//        Cone.Flavor [] balls ={Cone.Flavor.STRACIATELLA,
-//                Cone.Flavor.BANANA, Cone.Flavor.CHOCOLATE, Cone.Flavor.LEMON, Cone.Flavor.MOKKA, Cone.Flavor.PISTACHE,
-//                Cone.Flavor.STRAWBERRY, Cone.Flavor.STRAWBERRY, Cone.Flavor.VANILLA};
-//
-//        Cone orderCone = orderCone(balls);
-//        if (Cone.Flavor.Cone.Flavor.VANILLA.equals(orderCone)) {
-//            System.out.println("EATING Cone VANILA" + "Price is " + (balls.length + 1.50));
-//        } else if (Cone.Flavor.Cone.Flavor.STRAWBERRY.equals(orderCone)) {
-//            System.out.println("Eating cone Strawberry " + "price is " + (balls.length * 1.50));
-//        } else if ( Cone.Flavor.CHOCOLATE.equals(orderCone)){
-//            System.out.println("EATING CONE CHOCOLATE " + "PRICE IS "+ (balls.length * 1.50));}
-//        else if (Cone.Flavor.BANANA.equals(orderCone)){
-//            System.out.println("EATING CONE BANANA " + "PRICE IS "+ (balls.length * 1.50));}
-//        else if (Cone.Flavor.LEMON.equals(orderCone)){
-//            System.out.println("EATING CONE LEMON " + "PRICE IS "+ (balls.length * 1.50));}
-//            else{
-//            System.out.println("noting");
-//
-
-
-
     }
 
     @Override
